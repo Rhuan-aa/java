5e38b90c7968c8f0a45bb5b6c463856920825d73
rhuanaa <rhuanandrey@gmail.com>
2025-02-11 18:09:35 -0300

    :bug: fix

---
+++ listas-nivelamento/src/main/java/br/edu/ifsp/list01/Ex03.java
@@ -0,0 +1,37 @@
+package br.edu.ifsp.list01;
+
+/*
+    Escrever um programa que, dado um ano válido qualquer, verifica se ele é bissexto ou não:
+
+    São bissextos todos os anos múltiplos de 400, p. ex: 1600, 2000, 2400, 2800...
+    São bissextos todos os múltiplos de 4 e não múltiplos de 100, p.ex: 1996, 2004, 2008, 2012, 2016…
+    Não são bissextos os demais anos.
+    Exemplos de entrada e saída esperada:
+
+    Exemplo 1: Entrada = 1600 | Saída = Ano bissexto
+    Exemplo 2: Entrada = 1997 | Saída = Ano nao bissexto
+    Exemplo 3: Entrada = 2000 | Saída = Ano bissexto
+    Exemplo 4: Entrada = 2016 | Saída = Ano bissexto
+    Exemplo 5: Entrada = 0 | Saída = Erro
+*/
+public class Ex03 {
+
+    public static void main(String[] args) {
+        //Leia o input
+        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        //Escreva o resultado da chamada do método compute() aqui
+    }
+
+
+    String compute(int input) {
+        if (input <= 0) return "Erro";
+
+        if (input%400 == 0){
+            return "Ano bissexto";
+        } else if (input%4 == 0 && !(input%100 == 0)){
+            return "Ano bissexto";
+        }
+
+        return "Ano nao bissexto";
+    }
+}
---
+++ listas-nivelamento/src/main/java/br/edu/ifsp/list01/Ex04.java
@@ -0,0 +1,42 @@
+package br.edu.ifsp.list01;
+
+/*
+    Osmar adora chocolates e vai para a loja com N dinheiro no bolso. O preço de cada chocolate é C.
+    A loja oferece um desconto: para cada M embalagens que ele dá para a loja, ele ganha um chocolate grátis.
+    Quantos chocolates Osmar consegue comer? Por exemplo:
+
+    Para N=10, C=2, M=5, ele pode comprar 5 chocolates por $10 e trocar as 5 embalagens por mais 1 chocolate,
+    fazendo com que o número total de chocolates que ele pode comer seja 6.
+    Faça um programa que leia inteiros N, C e M e imprima a quantidade de chocolates que Osmar pode comer.
+    C e M são inteiros positivos.
+
+    Entrada	Saída
+    10      6
+    2
+    5
+ */
+public class Ex04 {
+
+    public static void main(String[] args) {
+        //Leia o input
+        //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        //Escreva o resultado da chamada do método compute() aqui
+    }
+
+    int compute(int n, int c, int m) {
+        int comprados, embalagens;
+
+        if (m <= 0 || n <= 0 || c <= 0) return -1;
+
+        comprados = n / c;
+        embalagens = comprados;
+
+        while (embalagens >= m){
+            int novasEmbalagens = embalagens / m;
+            comprados += novasEmbalagens;
+            embalagens = (embalagens%m) + novasEmbalagens;
+        }
+
+        return comprados;
+    }
+}
